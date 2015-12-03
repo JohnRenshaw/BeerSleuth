30c9b8344b8dbeaf8139cf767e30334c469f8214
JohnRenshaw <johnsrenshaw@gmail.com>
2015-12-02 23:10:31 -0800

    more caching

--- beersleuth_web.py
+++ beersleuth_web.py
@@ -5,6 +5,8 @@ import json
 import pandas as pd
 import shutil
 import time
+from collections import Counter
+import cPickle as pickle
 from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.exc import DataError, IntegrityError
@@ -13,17 +15,14 @@ from pyspark import SQLContext, HiveContext
 from pyspark.mllib.recommendation import MatrixFactorizationModel
 import beer_spark as modeling
 
+os.environ["SPARK_HOME"] = "/home/ubuntu/spark-1.5.2-bin-hadoop2.6"
 engine = create_engine('postgresql://postgres:123@localhost:5432/beersleuth')
 cursor = engine.connect()
 q = cursor.execute('SELECT DISTINCT beer, beer_id FROM mt3beers')
 beer_dict = dict([(beer[0].encode('utf-8', "ignore"), int(beer[1])) for beer in q])
 rev_beer_dict = dict([(v, k) for k,v in beer_dict.iteritems()])
-    #start spark
-os.environ["SPARK_HOME"] = "/home/ubuntu/spark-1.5.2-bin-hadoop2.6"
-conf = SparkConf().setAppName("BeerSleuthALS").set("spark.executor.memory", "4g")
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-path = tempfile.mkdtemp()
+
+
 
 
 
@@ -48,16 +47,35 @@ def get_ratings():
 
 @app.route('/fitting')
 def ALS_fit():
+    usern = request.args.get('usern')
+    users_df = pd.read_sql_query('''SELECT DISTINCT mt3ratings.user, user_id FROM mt3ratings WHERE appdata = 1''', engine)
+    if usern not in users_df['user'].values:
+        return_str =  "can't find user"
+        return jsonify(result = return_str)
+    user_id = users_df.user_id[users_df.user == usern].values[0]
     try: key = request.args.get('key')
     except NameError: key = 'e'
     if key == 'abcd':
+            #start spark
+
+        try:
+             conf = SparkConf().setAppName("BeerSleuthALS").set("spark.executor.memory", "4g")
+             sc = SparkContext(conf=conf)
+        except ValueError: pass
+        sqlContext = SQLContext(sc)
         ratings_sqldf = modeling.get_item_user_rev_from_pg(engine, sqlContext)
         sqlContext.registerDataFrameAsTable(ratings_sqldf, "ratings")
         print('fitting model')
-	model = modeling.fit_final_model(ratings_sqldf)
-	print('save model')
-        model.save(sc, path)
-	print('done')
+    	model = modeling.fit_final_model(ratings_sqldf)
+        beer_ids = beer_dict.values()
+        to_predict = zip([user_id]*len(beer_ids), beer_ids)
+        user_preds = model.predictAll(sc.parallelize(to_predict)).collect()
+        print('got preds')
+        preds = Counter({x[1]: x[2] for x in user_preds})
+        with open('%s_preds.pkl'%user_id,'wb') as f:
+            pickle.dump(preds, f)
+        print('done')
+        sc.stop()
         return jsonify(result="Model training complete, you may now get predictions")
 
 
@@ -123,28 +141,35 @@ def prediction():
                 return jsonify(result = return_str)
 
             if beer_p not in beer_dict.keys():
-		return_str = "can't find beer"
-		return jsonify(result = return_str)
+        		return_str = "can't find beer"
+        		return jsonify(result = return_str)
             user_id = users_df.user_id[users_df.user == user_p].values[0]
             beer_id = beer_dict[beer_p]
-            print user_p, beer_p, user_id, beer_id
-            model = MatrixFactorizationModel.load(sc, path)
-            pred = model.predict(user_id, beer_id)
-            return_str = "Prediction: %0.1f"%pred
+            with open('%s_preds.pkl'%user_id,'rb') as f:
+                preds = pickle.load(f)
+            return_str = "Prediction: %0.1f"%preds[beer_id]
             return jsonify(result = return_str)
 
 @app.route('/top20')
-def prediction():
+def top20():
     i=0
     user_p = request.args.get('user_p')
+    users_df = pd.read_sql_query('''SELECT DISTINCT mt3ratings.user, user_id FROM mt3ratings WHERE appdata = 1''', engine)
+    if user_p not in users_df['user'].values:
+        return_str =  "can't find user"
+        return jsonify(result = return_str)
     try: key = request.args.get('key')
     except NameError: key = 'e'
     if key == 'abcd':
         user_id = users_df.user_id[users_df.user == user_p].values[0]
-        model = MatrixFactorizationModel.load(sc, path)
-        pred_df = pd.DataFrame(model.recommendProducts(user_id, 20), columns = ['user', 'beer_id', 'rating'])
+        with open('%s_preds.pkl'%user_id,'rb') as f:
+            preds = pickle.load(f)
+        rank = range(1,21)
+        beers = [rev_beer_dict[k[0]] for k in preds.most_common(20)]
+        pred_df = pd.DataFrame({'rank':rank,'beer':beers})
         return  jsonify(result = pred_df.to_html(index=False, col_space = "50%", classes = "table-hover"))
 
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',  debug=False)
+ ##  app.run(debug = True)
